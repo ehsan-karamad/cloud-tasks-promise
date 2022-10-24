0833474c22ee118fefb5c7222bb211817d1f1933
Ehsan Karamad <ekaramad@google.com>
2022-10-24 14:12:52 -0400

    Update the library to expose `call` and `respond`

--- async-call.js
+++ async-call.js
@@ -1,63 +1,71 @@
 const { CloudTasksClient } = require('@google-cloud/tasks');
 const { randomUUID } = require('crypto');
 
+const CLOUD_TASKS_HEADER_TASK_NAME = 'X-CloudTasks-TaskName';
+let client = new CloudTasksClient();
+let PROJECT = 'ekaramad-playground';
+let LOCATION = 'us-central1';
+let POLLING_INTERVAL = 2000;
 
-class AsyncCall {
-    #projectId;
-    #locationId;
-    #client;
-    constructor(projectId, locationId) {
-        this.#projectId = projectId;
-        this.#locationId = locationId;
-        this.#client = new CloudTasksClient();
-    }
+exports.init = (config) => {
+    PROJECT = config.projectId || PROJECT;
+    LOCATION = config.locationId || LOCATION;
+    POLLING_INTERVAL = config.pollingInterval || pollingInterval;
+    client = config.client || client;
+};
 
-    // Makes an outbound asynchronous HttpCall.
-    async call(httpCall, queueId, responseQueue) {
-        const client = this.#client;
-        const projectId = this.#projectId;
-        const locationId = this.#locationId;
-        const opId = randomUUID();
-        const headers = httpCall.headers || {}
-        headers['Content-Type'] = 'application/json';
-        const body = httpCall.body || {};
-        if (!httpCall.url) {
-            throw new Error('URL is missing.');
-        } 
-        const scheduleTime = {}
-        if (httpCall.delaySec) {
-            scheduleTime.seconds = Math.round(Date.now() / 1000) + delaySec;
-        }
-        queueId = queueId || 'default';
-        responseQueue = responseQueue || 'default';
-        return new Promise(async (resolve, reject) => {
-            const taskName = client.taskPath(projectId, locationId, queueId, opId);
-            const respTaskId = `resp-${opId}`;
-            const responseTaskName = client.taskPath(projectId, locationId, responseQueue, respTaskId);
-            const queueName = client.queuePath(projectId, locationId, queueId);
-            await client.createTask({parent: queueName, task : {
-                httpRequest : {
-                    url: httpCall.url,
-                    headers: httpCall.headers,
-                    body: httpCall.body
-                },
-                scheduleTime : scheduleTime,
-                name: taskName
-            }});
-            setInterval( async () => {
-                try {
-                    const response = await client.getTask({name: responseTaskName, responseView: 'FULL'});
-                    resolve({
-                        body: response[0].httpRequest.body.toString(),
-                        headers: response[0].httpRequest.headers
-                    });
-                } catch(e) {
-                    // Ignore;
-                    console.log(`Still no resposne for ${opId}...`);
-                }
-            }, 1000);
-        });
-    }
+function replaceQueueId(taskName, queueId) {
+    const parts = taskName.split('/');
+    parts[5] = queueId;
+    return parts.join('/');
 }
 
-exports.AsyncCall = AsyncCall;
+exports.respond = (url, payload, incomingRequest, respQueueId) => {
+    const reqTaskName = incomingRequest.headers[CLOUD_TASKS_HEADER_TASK_NAME];
+    const taskName = replaceQueueId(reqTaskName, respQueueId);
+    return client.createTask({
+        parent: client.queuePath(PROJECT, LOCATION, respQueueId),
+        task: {
+            name: taskName,
+            httpRequest: {
+                url: url,
+                body: Buffer.from(JSON.stringify(payload)).toString('base64')
+            },
+            scheduleTime: {
+                seconds: Math.floor(Date.now() / 1000) + 1800000, // 30 min from now
+            }
+        }
+    });
+};
+
+exports.call = (url,
+    payload,
+    outboundQueue,
+    inboundQueue,
+    oidcToken) => {
+    return new Promise((resolve) => {
+        client.createTask({
+            parent: client.queuePath(PROJECT, LOCATION, outboundQueue),
+            task: {
+                httpRequest: {
+                    url: url,
+                    body: Buffer.from(JSON.stringify(payload)).toString('base64'),
+                    oidcToken: oidcToken
+                }
+            }
+        }).then(createdTask => {
+            const taskName = replaceQueueId(createdTask[0].name, inboundQueue);
+            const interval = setInterval(async () => {
+                try {
+                    const response = await client.getTask({ name: taskName });
+                    response[0].httpRequest.body = JSON.parse(response[0].httpRequest.body);
+                    resolve(response[0].httpRequest);
+                    clearInterval(interval);
+                } catch (e) {
+                    // Ignore errors.
+                    console.log(`Waiting for task: ${taskName}`);
+                }
+            }, POLLING_INTERVAL);
+        });
+    });
+};
--- test.js
+++ test.js
@@ -1,22 +1,39 @@
-const {AsyncCall} = require('./async-call');
+const { init, call, respond } = require('./async-call.js');
+const { expect } = require('chai');
 
-const URL = 'https://www.google.ca';
+class CloudTasksMock {
+    tasks = [];
+    queuePath(project, location, queue) {
+        return `projects/${project}/locations/${location}/queues/${queue}`
+    }
+    createTask(req) {
+        req.task.name = req.parent + '/tasks/taskId';
+        this.tasks.push(req);
+        return Promise.resolve([req.task]);
+    }
+    getTask(req) {
+        const task = this.tasks.find((t) => t.task.name === req.name);
+        if (task) {
+            const newTask = JSON.parse(JSON.stringify(task.task));
+            newTask.httpRequest.body = Buffer.from(newTask.httpRequest.body, 'base64').toString('ascii');
+            return Promise.resolve([newTask]);
+        }
+        return Promise.reject('task not found');
+    }
+};
 
-const stub = new AsyncCall('ekaramad-playground', 'us-central1');
-
-async function main() {
-    const response = await stub.call(
-        {
-            url: URL,
-            headers : {
-                'foo' : 'bar'
-            },
-            body: { name: "Ehsan"},
-        },
-        'default',
-        'response-default'
-    );
-    console.log('response');
-}
-
-main().then( () => console.log("done"));
+describe('call/respond tests', () => {
+    const client = new CloudTasksMock();
+    init({ client: client, projectId: 'project', locationId: 'location', 'pollingInterval': 100 });
+    it('Test request response path', async () => {
+        const pendingResponse = call('test-url', '123', 'request-queue', 'response-queue');
+        const fakeRequest = {
+            headers: {
+                'X-CloudTasks-TaskName': client.queuePath('project', 'location', 'request-queue') + '/tasks/taskId'
+            }
+        }
+        await respond('unuused', '456', fakeRequest, 'response-queue')
+        const response = await pendingResponse;
+        expect(response.body).to.equal('456');
+    });
+});
